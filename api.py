221ef44b84472f8f9714e952483acae711156ed2
DeadSakura <taoda001@gmail.com>
2013-12-25 16:03:43 -0600

    Run like twip T/O mode. Added logging.

--- api.py
+++ api.py
@@ -5,6 +5,7 @@ import httprober
 
 import urlparse
 import os
+import logging
 import webapp2
 import jinja2
 from google.appengine.ext import ndb
@@ -41,6 +42,9 @@ class CallbackHandler(webapp2.RequestHandler):
 
         # If we cannot get anything, the token in callback is illgeal.
         if saved_token == None:
+
+            logging.error("Request token %s is invaild. Aborted." % callback_token['oauth_token'])
+
             template_values = {
             'error_code': 'Internal Error',
             'error_reason': 'Could not found a matched request token from callback.',
@@ -49,17 +53,27 @@ class CallbackHandler(webapp2.RequestHandler):
             self.response.write(template.render(template_values))
             return
 
+
+
+
         # create token from saved request token and received verifier
+        logging.debug("Retrieved saved request token pair: %s/%s" % (saved_token.key, saved_token.secret))
+        logging.debug("Retrieved request token verifier: %s" % callback_token['oauth_verifier'])
+
         token = oauth.Token(saved_token.key,
-                        saved_token.secret)
+                        saved_token.secret)        
         token.set_verifier(callback_token['oauth_verifier'])
         # get access token from server.
+        logging.debug("Retrive access token/secret pair.")
+
         consumer = oauth.Consumer(consumer_key, consumer_secret)
         client = oauth.Client(consumer, token)
         resp, content = client.request(access_token_url, "POST")
         # parse token, and look up whether a same key/secret pair is found.
         access_token = dict(urlparse.parse_qsl(content))
         exist_token = Token.query(Token.key == access_token['oauth_token']).get()
+
+        logging.info("Retrived access token/secret pair: %s/%s" % (access_token['oauth_token'], access_token['oauth_token_secret']))
 
         # Add that UID with key/secret combination.
         # When we found the same key/secret combination with a different UID,
@@ -68,8 +82,12 @@ class CallbackHandler(webapp2.RequestHandler):
         saved_token.secret = access_token['oauth_token_secret']
         saved_token.put()
 
+        logging.debug("Saved access token/secret pair: %s/%s" % (access_token['oauth_token'], access_token['oauth_token_secret']))
 
         if exist_token and exist_token.secret == access_token['oauth_token_secret']:
+
+            logging.info("Same access token/secret pair found.")
+
             title = 'Another UID added'
             message = 'You now have more names.'
             detail = 'We have added a custom URL for you, besides, we found you already have a custom URL.\n'
@@ -77,6 +95,9 @@ class CallbackHandler(webapp2.RequestHandler):
             import_message = 'However, please keep them privately. Anyone can send tweet using your URL.'
             
         else:
+
+            logging.info("No same access token/secret pair found.")
+
             title = 'Custom URL created'
             message = 'You could use PTAP now.'
             detail = 'Please keep your custom URL privately.'
@@ -84,6 +105,7 @@ class CallbackHandler(webapp2.RequestHandler):
 
         custom_url = 'https://' + self.request.host + '/api/' + saved_token.UID + '/'
 
+        logging.debug("Parse result html file.")
 
         template_values = {
             'title': title,
@@ -94,21 +116,32 @@ class CallbackHandler(webapp2.RequestHandler):
             }
         template = JINJA_ENVIRONMENT.get_template('success.html')
         self.response.write(template.render(template_values))
+
+        logging.debug("Parsing completed.")
         
 class AuthorizeAPI(webapp2.RequestHandler):
     """Authroize PTAP."""
 
     def post(self):
+
+        logging.debug("Check UID.")
         # determine if UID exist. if so, redirect back to UID setting page.
         id = self.request.get('UID')
         isUIDexist = Token.query(Token.key == id).get()
         if isUIDexist:
+
+            logging.info("%s is found in db. Redirecting back." % id)
+
             self.redirect('/api/authorize_me')
             return
+
+        logging.debug("Request request token.")
 
         consumer = oauth.Consumer(consumer_key, consumer_secret)
         client = oauth.Client(consumer)
         resp, content = client.request(request_token_url, "GET")
+
+
         if resp['status'] != '200':
             template_values = {
             'error_code': resp['status'],
@@ -116,11 +149,18 @@ class AuthorizeAPI(webapp2.RequestHandler):
             }
             template = JINJA_ENVIRONMENT.get_template('error.html')
             self.response.write(template.render(template_values))
+
+            logging.error("Error occurs while requesting request token, code: %s" % resp['status'])
         else:
             request_token = dict(urlparse.parse_qsl(content))
             self.redirect("%s?oauth_token=%s" % (authorize_url, request_token['oauth_token']))
+
+            logging.info("Retrieved request token/secret pair:" + request_token['oauth_token'] + "/" + request_token['oauth_token_secret'])
+
             token = Token(UID = id, key = request_token['oauth_token'], secret = request_token['oauth_token_secret'])
             token.put()
+
+            logging.info("Saved request token/secret pair in db.")
         
 class APIproxy(webapp2.RequestHandler):
     """docstring for APIproxy"""
@@ -140,29 +180,86 @@ class APIproxy(webapp2.RequestHandler):
         try:
             uid, path = raw_path.split('/', 1)[0], raw_path.split('/', 1)[1]
         except IndexError, e:
+            logging.error(e)
             self.abort(400)
         
         # try to retrieve saved token by uid. If we could not retrieve one, we can do nothing.
         token_query = Token.query(Token.UID == uid).get()
         if token_query == None:
+            logging.error("Cound not find UID. Aborted.")
             self.abort(400)
 
         # request information from server.
+         
+        logging.info("Request info from server.")
+
         token = oauth.Token(token_query.key, token_query.secret)
         consumer = oauth.Consumer(consumer_key, consumer_secret)
         client = oauth.Client(consumer, token)
         request_url = twitter_base_url + path
+
+        logging.debug("Request address: %s, Method: %s" % (request_url, method))
+
         server_res, server_content = client.request(request_url, method, self.request.body, self.request.headers)
 
         # clear response, add retrieved information.
         self.response.clear()
 
-
+        logging.info("Add received headers.")
         for key in server_res.keys():
             self.response.headers.add(key, str(server_res.get(key)))
             #self.response.write(key + '=' + server_res.get(key) + '<br>')
 
+        logging.info("Add received body.")
         self.response.write(server_content)
+
+
+class TransparentProxy(webapp2.RequestHandler):
+    """Transparent forward all request to Twitter"""
+
+    def get(self):
+        self.do_proxy('GET')
+
+    def post(self):
+        self.do_proxy('POST')
+
+    def do_proxy(self, method):
+        # parse requested path and query string.
+        parsed_result = urlparse.urlparse(self.request.url)
+        path, qs = parsed_result.path[6:], parsed_result.query
+
+        logging.info("T Mode, requested: " + path + ", " + "QueryString is " + qs)
+
+        import httplib
+        headers = []
+        for key in self.request.headers:
+
+            # skip some headers to silence GAE.
+            if key == 'Content_Length' or key == 'Host':
+                continue
+            headers.append((key, self.request.headers[key]))
+
+        conn = httplib.HTTPSConnection('api.twitter.com')
+
+        try:
+            conn.request(method, path + '?' + qs, self.request.body, dict(headers))
+            res = conn.getresponse()
+            logging.info("Got response from Twitter.")
+            logging.info("Add HTTP headers.")
+            for key, value in res.getheaders():
+                # skip some headers to silence GAE.
+                if key == 'Host':
+                    continue
+                self.response.headers.add(key, value)
+                
+            self.response.write(res.read())
+            logging.info("Response body: " + res.read())
+        except Exception, e:
+            logging.error(e)
+            self.abort(500)
+
+
+
 
 
 
@@ -177,5 +274,6 @@ class APIproxy(webapp2.RequestHandler):
 app = webapp2.WSGIApplication([
     ('/api/authorize_me', AuthorizeAPI),
     (r'/api/callback\?*', CallbackHandler),
+    (r'/api/t/.*', TransparentProxy),
     (r'/api/.*', APIproxy),
-], debug=True)
+], debug=False)
